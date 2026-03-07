e412e67b23f0c828e5183360ea9643455b19854f
mmulekya <kizitohamisi61@gmail.com>
2026-03-07 21:11:00 +0300

    Create save_update.php

---
+++ save_update.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include("config/database.php");
+
+$constructor_id = $_SESSION['user_id'];
+$project_id = $_POST['project_id'];
+$update_text = $_POST['update_text'];
+
+$sql = "INSERT INTO project_updates (project_id, constructor_id, update_text)
+VALUES ('$project_id','$constructor_id','$update_text')";
+
+mysqli_query($conn,$sql);
+
+header("Location: project_details.php?id=$project_id");
+?>
