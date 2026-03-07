97d6af2c47ff7465fe45ab30f6dee7a7c773d459
mmulekya <kizitohamisi61@gmail.com>
2026-03-07 13:52:05 +0300

    Create login_process.php

---
+++ login_process.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+include("config/database.php");
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE email='$email'";
+$result = mysqli_query($conn,$sql);
+
+if(mysqli_num_rows($result) == 1){
+
+$user = mysqli_fetch_assoc($result);
+
+if(password_verify($password,$user['password'])){
+
+$_SESSION['user_id'] = $user['id'];
+$_SESSION['name'] = $user['name'];
+$_SESSION['role'] = $user['role'];
+
+header("Location: dashboard.php");
+
+}else{
+
+echo "Wrong password";
+
+}
+
+}else{
+
+echo "User not found";
+
+}
+
+?>
