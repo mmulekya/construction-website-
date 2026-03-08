eb55345dbbb0cb38c87dfa8d175a0dc65248a503
mmulekya <kizitohamisi61@gmail.com>
2026-03-08 12:44:31 +0300

    Create planner_result.php

---
+++ planner_result.php
@@ -0,0 +1,21 @@
+<?php
+
+$type = $_POST['type'];
+$size = $_POST['size'];
+$floors = $_POST['floors'];
+
+echo "<h2>AI Generated Construction Plan</h2>";
+
+echo "<ol>";
+
+echo "<li>Site Preparation</li>";
+echo "<li>Foundation Construction</li>";
+echo "<li>Structure Building</li>";
+echo "<li>Roof Installation</li>";
+echo "<li>Electrical & Plumbing</li>";
+echo "<li>Interior Finishing</li>";
+echo "<li>Final Inspection</li>";
+
+echo "</ol>";
+
+?>
