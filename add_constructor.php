f418f26b4656c315cb5fad9597ac6b470c9b8117
mmulekya <kizitohamisi61@gmail.com>
2026-03-11 13:52:50 +0300

    Update add_constructor.php

---
+++ add_constructor.php
@@ -0,0 +1,29 @@
+<?php
+include("includes/header.php");
+
+if(!isset($_SESSION['user_id'])){
+header("Location: login.php");
+}
+?>
+
+<h2>Create Constructor Profile</h2>
+
+<form action="save_constructor.php" method="POST">
+
+<label>Specialization</label>
+<input type="text" name="specialization" placeholder="e.g Masonry, Electrical">
+
+<label>Years of Experience</label>
+<input type="number" name="experience">
+
+<label>City</label>
+<input type="text" name="city">
+
+<label>Bio</label>
+<textarea name="bio"></textarea>
+
+<button type="submit">Save Profile</button>
+
+</form>
+
+<?php include("includes/footer.php"); ?>
