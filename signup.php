0e87e60d19fe7bbed8b01dc0e0bd813e168e22c9
mmulekya <kizitohamisi61@gmail.com>
2026-03-11 11:50:00 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,1 +1,26 @@
->
+ <?php include("includes/header.php"); ?>
+
+<h2>Create Account</h2>
+
+<form action="signup_process.php" method="POST">
+
+<label>Name</label>
+<input type="text" name="name" required>
+
+<label>Email</label>
+<input type="email" name="email" required>
+
+<label>Password</label>
+<input type="password" name="password" required>
+
+<label>Role</label>
+<select name="role">
+<option value="client">Client</option>
+<option value="constructor">Constructor</option>
+</select>
+
+<button type="submit">Signup</button>
+
+</form>
+
+<?php include("includes/footer.php"); ?>
