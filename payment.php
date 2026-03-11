99132b74f9ac87a94d92d4d4ea7680e5596c2873
mmulekya <kizitohamisi61@gmail.com>
2026-03-11 16:51:52 +0300

    Create payment.php

---
+++ payment.php
@@ -0,0 +1,34 @@
+<?php
+include("includes/header.php");
+
+if(!isset($_SESSION['user_id'])){
+    header("Location: login.php");
+}
+
+$project_id = $_GET['project_id'];
+$constructor_id = $_GET['constructor_id'];
+
+?>
+
+<h2>Make Payment</h2>
+
+<form action="process_payment.php" method="POST">
+
+<input type="hidden" name="project_id" value="<?php echo $project_id; ?>">
+<input type="hidden" name="constructor_id" value="<?php echo $constructor_id; ?>">
+
+<label>Amount (USD)</label>
+<input type="number" name="amount" required>
+
+<label>Payment Method</label>
+<select name="method">
+<option value="mpesa">Mpesa</option>
+<option value="paypal">PayPal</option>
+<option value="bank">Bank Transfer</option>
+</select>
+
+<button type="submit">Pay Now</button>
+
+</form>
+
+<?php include("includes/footer.php"); ?>
