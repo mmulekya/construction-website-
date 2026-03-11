62050d0f3d5be1c523870a7c129904af2cf18b5b
mmulekya <kizitohamisi61@gmail.com>
2026-03-11 16:43:36 +0300

    Update save_review.php

--- save_review.php
+++ save_review.php
@@ -1,17 +1,27 @@
 <?php
+
 session_start();
+
 include("config/database.php");
+
+$client_id = $_SESSION['user_id'];
 
 $constructor_id = $_POST['constructor_id'];
 $project_id = $_POST['project_id'];
-$client_id = $_SESSION['user_id'];
 $rating = $_POST['rating'];
 $review = $_POST['review'];
 
-$sql = "INSERT INTO reviews (constructor_id, client_id, project_id, rating, review)
+$sql = "INSERT INTO reviews (constructor_id,client_id,project_id,rating,review)
 VALUES ('$constructor_id','$client_id','$project_id','$rating','$review')";
 
-mysqli_query($conn,$sql);
+if(mysqli_query($conn,$sql)){
 
-echo "Review submitted successfully!";
+header("Location: constructors.php");
+
+}else{
+
+echo "Review failed";
+
+}
+
 ?>
