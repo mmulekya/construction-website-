14345073983cad704f8eeee1d873dbc6e5cf47b6
mmulekya <kizitohamisi61@gmail.com>
2026-03-11 11:59:30 +0300

    Update login_process.php

---
+++ login_process.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+include("config/database.php");
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE email='$email'";
+
+$result = mysqli_query($conn,$sql);
+
+$user = mysqli_fetch_assoc($result);
+
+if($user && password_verify($password,$user['password'])){
+
+$_SESSION['user_id'] = $user['id'];
+$_SESSION['name'] = $user['name'];
+$_SESSION['role'] = $user['role'];
+
+header("Location: dashboard.php");
+
+}else{
+
+echo "Invalid login";
+
+}
+
+?>
