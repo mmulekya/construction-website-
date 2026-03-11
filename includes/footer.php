203eada0f0040592f591bd6225d8420fa96b0ae3
mmulekya <kizitohamisi61@gmail.com>
2026-03-11 11:34:44 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,26 @@
+</div>
 
+<footer>
+
+<p>© <?php echo date("Y"); ?> BuildSmart Platform</p>
+
+</footer>
+
+<script>
+
+if ('serviceWorker' in navigator) {
+
+navigator.serviceWorker.register('service-worker.js')
+
+.then(function() {
+
+console.log("Service Worker Registered");
+
+});
+
+}
+
+</script>
+
+</body>
+</html>
