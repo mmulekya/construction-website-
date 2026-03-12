ddfd60fa305f1d7f1b1e81a1ef5efe82b939d44d
mmulekya <kizitohamisi61@gmail.com>
2026-03-12 12:26:46 +0300

    Update save_bid.php

--- save_bid.php
+++ save_bid.php
@@ -23,4 +23,19 @@ echo "Error submitting bid";
 
 }
 
+// notify project owner
+
+$project_sql = "SELECT client_id FROM projects WHERE id='$project_id'";
+$project_result = mysqli_query($conn,$project_sql);
+$project = mysqli_fetch_assoc($project_result);
+
+$client_id = $project['client_id'];
+
+$message = "A constructor placed a bid on your project.";
+$link = "project_details.php?id=".$project_id;
+
+$notify_sql = "INSERT INTO notifications (user_id,message,link)
+VALUES ('$client_id','$message','$link')";
+
+mysqli_query($conn,$notify_sql);
 ?>
