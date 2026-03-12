27d8036b3912db97dfb50bbc102c6db35e785c8d
mmulekya <kizitohamisi61@gmail.com>
2026-03-12 15:27:31 +0300

    Update constructors.php

--- constructors.php
+++ constructors.php
@@ -2,6 +2,22 @@
 include("includes/header.php");
 include("config/database.php");
 
+<form method="GET">
+
+<input type="text" name="city" placeholder="Search by city">
+
+<select name="specialization">
+<option value="">All Specializations</option>
+<option value="architect">Architect</option>
+<option value="engineer">Engineer</option>
+<option value="builder">Builder</option>
+<option value="contractor">Contractor</option>
+</select>
+
+<button type="submit">Search</button>
+
+</form>
+
 $sql = "SELECT users.name, constructors.specialization, constructors.experience, constructors.city, constructors.bio
 FROM constructors
 JOIN users ON constructors.user_id = users.id";
