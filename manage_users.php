a511232cd3b436f1a8eefdfa349d964fa2ce5ccf
mmulekya <kizitohamisi61@gmail.com>
2026-03-12 14:15:11 +0300

    Update manage_users.php

--- manage_users.php
+++ manage_users.php
@@ -1,13 +1,30 @@
 <?php
+
+include("includes/header.php");
 include("config/database.php");
 
 $sql = "SELECT * FROM users";
+
 $result = mysqli_query($conn,$sql);
 
-echo "<h2>All Users</h2>";
-
-while($row = mysqli_fetch_assoc($result)){
-echo $row['name']." - ".$row['role'];
-echo "<br>";
-}
 ?>
+
+<h2>Manage Users</h2>
+
+<?php while($row = mysqli_fetch_assoc($result)){ ?>
+
+<div class="card">
+
+<p><b>Name:</b> <?php echo $row['name']; ?></p>
+
+<p><b>Email:</b> <?php echo $row['email']; ?></p>
+
+<p><b>Role:</b> <?php echo $row['role']; ?></p>
+
+</div>
+
+<?php } ?>
+
+<?php include("includes/footer.php"); ?>
+
+
