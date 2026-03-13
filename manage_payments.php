3c0e5669ddeac9241656e1dab2110f8f35a4a3d4
mmulekya <kizitohamisi61@gmail.com>
2026-03-13 22:54:30 +0300

    Update manage_payments.php

--- manage_payments.php
+++ manage_payments.php
@@ -1,25 +1,26 @@
 <?php
-
 include("includes/header.php");
+include("includes/auth.php");
 include("config/database.php");
 
-$sql = "SELECT * FROM payments";
-
-$result = mysqli_query($conn,$sql);
-
+$result = $conn->query("SELECT * FROM payments ORDER BY id DESC");
 ?>
 
-<h2>Manage Payments</h2>
+<h2>All Platform Payments</h2>
 
-<?php while($row = mysqli_fetch_assoc($result)){ ?>
+<?php while($row = $result->fetch_assoc()){ ?>
 
 <div class="card">
 
-<p><b>Project:</b> <?php echo $row['project_id']; ?></p>
+<p>User ID: <?php echo htmlspecialchars($row['user_id']); ?></p>
 
-<p><b>Amount:</b> $<?php echo $row['amount']; ?></p>
+<p>Project ID: <?php echo htmlspecialchars($row['project_id']); ?></p>
 
-<p><b>Status:</b> <?php echo $row['status']; ?></p>
+<p>Amount: $<?php echo htmlspecialchars($row['amount']); ?></p>
+
+<p>Method: <?php echo htmlspecialchars($row['method']); ?></p>
+
+<p>Status: <?php echo htmlspecialchars($row['status']); ?></p>
 
 </div>
 
