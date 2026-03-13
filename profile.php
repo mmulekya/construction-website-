9aa95d62598315560b72c5d0ad90160000146dce
mmulekya <kizitohamisi61@gmail.com>
2026-03-13 14:16:58 +0300

    Update profile.php

--- profile.php
+++ profile.php
@@ -23,4 +23,27 @@ $user = mysqli_fetch_assoc($result);
 
 <p><b>About:</b> <?php echo $user['bio']; ?></p>
 
+<h3>Portfolio</h3>
+
+<?php
+
+$sql = "SELECT * FROM portfolios WHERE user_id='$id'";
+$result = mysqli_query($conn,$sql);
+
+while($row = mysqli_fetch_assoc($result)){
+
+?>
+
+<div class="card">
+
+<h4><?php echo $row['title']; ?></h4>
+
+<img src="uploads/<?php echo $row['image']; ?>" width="250">
+
+<p><?php echo $row['description']; ?></p>
+
+</div>
+
+<?php } ?>
+
 <?php include("includes/footer.php"); ?>
