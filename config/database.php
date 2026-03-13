f7becc6a5bb0c39d76ead465db01f679cb7d46ca
mmulekya <kizitohamisi61@gmail.com>
2026-03-13 22:18:24 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,5 +1,29 @@
 <?php
 
+$host = "localhost";
+$dbname = "buildsmart";
+$username = "root";
+$password = "";
+
+try {
+
+$conn = new mysqli($host,$username,$password,$dbname);
+
+if ($conn->connect_error) {
+die("Database connection failed");
+}
+
+$conn->set_charset("utf8mb4");
+
+} catch(Exception $e){
+
+die("Database error");
+
+}
+
+?>
+<?php
+
 $host = "sql107.infinityfree.com";
 $username = "ifo_41039562";
 $password = "Jan36aPr20x";
