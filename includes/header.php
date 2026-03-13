a4a0137d9cd03a2857cd9255dfb4ae67d9499d7b
mmulekya <kizitohamisi61@gmail.com>
2026-03-13 16:28:25 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -26,37 +26,10 @@ session_start();
 <h2>🏗️ BuildSmart</h2>
 </div>
 
-<nav>
 
-<a href="index.php">Home</a>
 
-<a href="constructors.php">Constructors</a>
 
-<a href="projects.php">Projects</a>
 
-<a href="ai_estimator.php">AI Estimator</a>
-
-<a href="ai_planner.php">AI Planner</a>
-
-<a href="notifications.php">🔔 Notifications</a>
-
-<a href="ai_chat.php">AI Assistant</a>
-
-<?php if(isset($_SESSION['user_id'])) { ?>
-
-<a href="dashboard.php">Dashboard</a>
-
-<a href="logout.php">Logout</a>
-
-<?php } else { ?>
-
-<a href="login.php">Login</a>
-
-<a href="signup.php">Signup</a>
-
-<?php } ?>
-
-</nav>
 
 </header>
 
