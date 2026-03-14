e78bed08f5df8aee3a2ace0ad0ddfd6bdb232fa2
mmulekya <kizitohamisi61@gmail.com>
2026-03-14 23:13:31 +0300

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -21,3 +21,20 @@ header("Location: chat.php?user=".$receiver_id);
 
 }
 ?>
+
+<?php
+
+require_once "includes/security.php";
+require_once "includes/message_rate_limit.php";
+require_login();
+
+$message = sanitize($_POST['message']);
+$receiver_id = intval($_POST['receiver_id']);
+
+$stmt = $conn->prepare("INSERT INTO messages (sender_id,receiver_id,message) VALUES (?,?,?)");
+$stmt->bind_param("iis",$_SESSION['user_id'],$receiver_id,$message);
+$stmt->execute();
+
+echo "Message sent";
+
+?>
