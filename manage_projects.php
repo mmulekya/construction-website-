34bfa94dad3a60b010f808ecb0dbff3e5f98f4ce
mmulekya <kizitohamisi61@gmail.com>
2026-03-14 09:04:05 +0300

    Update manage_projects.php

--- manage_projects.php
+++ manage_projects.php
@@ -1,28 +1,31 @@
 <?php
-
 include("includes/header.php");
 include("config/database.php");
+include("includes/auth.php");
 
-$sql = "SELECT * FROM projects";
+if($_SESSION['role']!="admin"){
+die("Access denied");
+}
 
-$result = mysqli_query($conn,$sql);
-
+$result = $conn->query("SELECT * FROM projects ORDER BY id DESC");
 ?>
 
 <h2>Manage Projects</h2>
 
-<?php while($row = mysqli_fetch_assoc($result)){ ?>
+<?php while($row=$result->fetch_assoc()){ ?>
 
 <div class="card">
 
-<h3><?php echo $row['title']; ?></h3>
+<h3><?php echo htmlspecialchars($row['title']); ?></h3>
 
-<p><?php echo $row['description']; ?></p>
+<p><?php echo htmlspecialchars($row['description']); ?></p>
 
-<p><b>Budget:</b> $<?php echo $row['budget']; ?></p>
+<a href="delete_project.php?id=<?php echo $row['id']; ?>">Delete</a>
 
 </div>
 
 <?php } ?>
 
 <?php include("includes/footer.php"); ?>
+
+
