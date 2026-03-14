77406cf64ce520a3cf1fd67e48b3ff6d45cf2281
mmulekya <kizitohamisi61@gmail.com>
2026-03-14 14:45:46 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,28 +1,41 @@
-<?php include("includes/header.php"); ?>
+<?php
+include("includes/security.php");
+include("includes/header.php");
+?>
 
 <h2>Create Account</h2>
 
-<form action="signup_process.php" method="POST">
+<form action="register_process.php" method="POST">
 
 <label>Name</label>
-<input type="text" name="name" required maxlength="100">
+<input type="text" name="name" required>
 
 <label>Email</label>
 <input type="email" name="email" required>
 
 <label>Password</label>
-<input type="password" name="password" required minlength="8">
+<input type="password" name="password" required>
 
-<label>Role</label>
-
-<select name="role">
-
+<label>Account Type</label>
+<select name="role" required>
 <option value="client">Client</option>
 <option value="constructor">Constructor</option>
+</select>
+
+<label>Country</label>
+<select name="country" required>
+
+<option value="Kenya">Kenya</option>
+<option value="Tanzania">Tanzania</option>
+<option value="Uganda">Uganda</option>
+<option value="Rwanda">Rwanda</option>
+<option value="USA">United States</option>
+<option value="UK">United Kingdom</option>
+<option value="India">India</option>
 
 </select>
 
-<button type="submit">Sign Up</button>
+<button type="submit">Register</button>
 
 </form>
 
