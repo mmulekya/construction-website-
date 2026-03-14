683ba7b9b42ca1a3ceddcd9bcfa90159a9600e71
mmulekya <kizitohamisi61@gmail.com>
2026-03-14 09:05:03 +0300

    Update manage_users.php

--- manage_users.php
+++ manage_users.php
@@ -1,30 +1,31 @@
 <?php
-
 include("includes/header.php");
 include("config/database.php");
+include("includes/auth.php");
 
-$sql = "SELECT * FROM users";
+if($_SESSION['role']!="admin"){
+die("Access denied");
+}
 
-$result = mysqli_query($conn,$sql);
-
+$result = $conn->query("SELECT id,name,email,role FROM users ORDER BY id DESC");
 ?>
 
 <h2>Manage Users</h2>
 
-<?php while($row = mysqli_fetch_assoc($result)){ ?>
+<?php while($row=$result->fetch_assoc()){ ?>
 
 <div class="card">
 
-<p><b>Name:</b> <?php echo $row['name']; ?></p>
+<p><b>Name:</b> <?php echo htmlspecialchars($row['name']); ?></p>
 
-<p><b>Email:</b> <?php echo $row['email']; ?></p>
+<p><b>Email:</b> <?php echo htmlspecialchars($row['email']); ?></p>
 
-<p><b>Role:</b> <?php echo $row['role']; ?></p>
+<p><b>Role:</b> <?php echo htmlspecialchars($row['role']); ?></p>
+
+<a href="delete_user.php?id=<?php echo $row['id']; ?>">Delete</a>
 
 </div>
 
 <?php } ?>
 
 <?php include("includes/footer.php"); ?>
-
-
