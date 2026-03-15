f9c05f8931d043db30839b2be63367730c0bf00b
mmulekya <kizitohamisi61@gmail.com>
2026-03-15 17:51:48 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,25 +1,33 @@
 </div>
 <footer>
 <p>© 2026 BuildSmart</p>
-<a href="blog.php">Blog</a>
-<a href="portfolio.php">Portfolio</a>
-<a href="contact.php">Contact</a>
+
+<?php
+// Include security to access session info
+include("includes/security.php");
+
+// Check if user is logged in
+if(is_logged_in()){ // assume this function is in security.php
+?>
+    <a href="blog.php">Blog</a>
+    <a href="portfolio.php">Portfolio</a>
+    <a href="contact.php">Contact</a>
+<?php } else { ?>
+    <!-- Show only public pages -->
+    <a href="privacy.php">Privacy Policy</a>
+<?php } ?>
+
+<!-- Always show Privacy link -->
 <a href="privacy.php">Privacy Policy</a>
 </footer>
+
 <script>
-
 if ('serviceWorker' in navigator) {
-
-navigator.serviceWorker.register('service-worker.js')
-
-.then(function() {
-
-console.log("Service Worker Registered");
-
-});
-
+    navigator.serviceWorker.register('service-worker.js')
+    .then(function() {
+        console.log("Service Worker Registered");
+    });
 }
-
 </script>
 
 </body>
