77ce449e2b621b3b8395e77de46ac78e652545b3
mmulekya <kizitohamisi61@gmail.com>
2026-03-16 16:46:01 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,24 +1,13 @@
 </div>
+
 <footer>
 <p>© 2026 BuildSmart</p>
 
-<?php
-// Include security to access session info
-include("includes/security.php");
-
-// Check if user is logged in
-if(is_logged_in()){ // assume this function is in security.php
-?>
-    <a href="blog.php">Blog</a>
-    <a href="portfolio.php">Portfolio</a>
-    <a href="contact.php">Contact</a>
-<?php } else { ?>
-    <!-- Show only public pages -->
-    <a href="privacy.php">Privacy Policy</a>
-<?php } ?>
-
-<!-- Always show Privacy link -->
+<a href="blog.php">Blog</a>
+<a href="portfolio.php">Portfolio</a>
+<a href="contact.php">Contact</a>
 <a href="privacy.php">Privacy Policy</a>
+
 </footer>
 
 <script>
