214b2d791e82225ac2e362459dd0d09cd0ead118
mmulekya <kizitohamisi61@gmail.com>
2026-03-16 19:08:09 +0300

    Update verify.php

--- verify.php
+++ verify.php
@@ -6,25 +6,16 @@ $message = "";
 if(isset($_GET['token'])){
     $token = $_GET['token'];
 
-    $stmt = $conn->prepare(
-        "SELECT id FROM users WHERE verification_token=? AND is_verified=0"
-    );
-
+    $stmt = $conn->prepare("SELECT id FROM users WHERE verification_token=? AND is_verified=0");
     $stmt->bind_param("s",$token);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if($result->num_rows == 1){
-
-        $stmt = $conn->prepare(
-            "UPDATE users SET is_verified=1, verification_token=NULL WHERE verification_token=?"
-        );
-
+        $stmt = $conn->prepare("UPDATE users SET is_verified=1, verification_token=NULL WHERE verification_token=?");
         $stmt->bind_param("s",$token);
         $stmt->execute();
-
         $message = "Account verified successfully. You can now login.";
-
     }else{
         $message = "Invalid or expired verification link.";
     }
