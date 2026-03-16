561739d93d0091e2a941b8db5454f25d0b0089ad
mmulekya <kizitohamisi61@gmail.com>
2026-03-16 11:27:42 +0300

    Update auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -1,14 +1,18 @@
 <?php
+include_once "security.php";
 
-if (session_status() === PHP_SESSION_NONE) {
-session_start();
+/* Require login */
+function require_auth(){
+    if(!isset($_SESSION['user_id'])){
+        header("Location: login.php");
+        exit();
+    }
 }
 
-if(!isset($_SESSION['user_id'])){
-
-header("Location: login.php");
-exit();
-
+/* Require admin */
+function require_admin(){
+    if(!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin'){
+        die("Access denied.");
+    }
 }
-
 ?>
