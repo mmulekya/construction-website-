920b1040b569be94b9860b1f33f870679130b127
mmulekya <kizitohamisi61@gmail.com>
2026-03-16 19:34:08 +0300

    Update csrf.php

--- includes/csrf.php
+++ includes/csrf.php
@@ -1,9 +1,5 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-/* Generate CSRF token */
+// Generate CSRF token
 function csrf_token(){
     if(empty($_SESSION['csrf_token'])){
         $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
@@ -11,12 +7,8 @@ function csrf_token(){
     return $_SESSION['csrf_token'];
 }
 
-/* Verify CSRF token */
+// Verify CSRF token
 function verify_csrf($token){
-    if(!isset($_SESSION['csrf_token'])){
-        return false;
-    }
-
-    return hash_equals($_SESSION['csrf_token'], $token);
+    return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'],$token);
 }
 ?>
