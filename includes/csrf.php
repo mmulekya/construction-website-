a8678f295490e3ddbf3fa38861128480fa0a1b96
mmulekya <kizitohamisi61@gmail.com>
2026-03-16 11:26:55 +0300

    Update csrf.php

--- includes/csrf.php
+++ includes/csrf.php
@@ -1,17 +1,21 @@
 <?php
-
-if (empty($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
 }
 
+/* Generate token */
 function csrf_token(){
+    if(empty($_SESSION['csrf_token'])){
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
     return $_SESSION['csrf_token'];
 }
 
+/* Verify token */
 function verify_csrf($token){
-    if ($token !== $_SESSION['csrf_token']) {
-        die("Invalid CSRF token");
+    if(!isset($_SESSION['csrf_token'])){
+        return false;
     }
+    return hash_equals($_SESSION['csrf_token'], $token);
 }
-
 ?>
